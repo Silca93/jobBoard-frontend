fbbe4e1200bef553d80fe473944373d2c3fa4ea3
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-11-06 11:42:38 +0100

    adding proper env settinggggg

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.ENV === 'production' ? 'https://jobboard-backend-6clh.onrender.com' : 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.PROD ? 'https://jobboard-backend-6clh.onrender.com' : 'http://localhost:5000';
 
 // const API_BASE_URL = ''
 export default API_BASE_URL;
